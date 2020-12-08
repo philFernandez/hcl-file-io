ea797acdd275468ddb95d3a5dbd5c8f92e6fe7e8
philFernandez <norcal618@gmail.com>
2020-12-07 22:52:11 -0800

    finish up

--- src/main/java/com/hcl/prac/ConsumeSerial.java
+++ src/main/java/com/hcl/prac/ConsumeSerial.java
@@ -8,12 +8,17 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ConsumeSerial {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+    	// Use list with Wild Card Generic parameter for reading in Student Objects from File
         List<?> students = new ArrayList<>();
+        // Open Serialized file with BufferedInputStream
+        System.out.println("Reading in Serialized Data File...");
         try (BufferedInputStream bis =
                 new BufferedInputStream(new FileInputStream("students.dat"))) {
             ObjectInputStream ois = new ObjectInputStream(bis);
+            // Read deserialized data file
             students = (List<?>) ois.readObject();
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
@@ -22,7 +27,8 @@ public class ConsumeSerial {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
+        System.out.println("\nStudent Objects Read and Deserialized from Data File");
+        System.out.println("------------------------------------------------------");
         students.forEach(student -> System.out.println(student));
 
     }
